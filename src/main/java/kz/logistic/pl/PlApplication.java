c600bb327c8e85b2be316c2b8bfae0ec238f2b8b
Myrzakanov Daniyar <dan_ntu@mail.ru>
2019-01-19 16:30:30 +0600

    Новая структура базы данных. Все переделано

---
+++ src/main/java/kz/logistic/pl/PlApplication.java
@@ -0,0 +1,15 @@
+package kz.logistic.pl;
+
+import kz.logistic.pl.configs.SwaggerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@Import({SwaggerConfig.class})
+@SpringBootApplication
+public class PlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlApplication.class, args);
+  }
+}
